bc15bed5fa53918386133290e85390ceb4a17d70
inseonchoi <inseonc@mz.co.kr>
2021-09-01 18:19:16 +0900

    Update multi or single

--- load-bigquery/Kylie/load_bigquery.py
+++ load-bigquery/Kylie/load_bigquery.py
@@ -29,42 +29,96 @@ def load_gcs_to_bigquery(client, uri, table_id):
     return load_job.result()  
 
 
-def load_bigquery_to_excel_sheets(INPUT_DATA_PATH, OUTPUT_DATA_PATH, PROJECT_ID, GOOGLE_SERVICE_ACCOUNT_FILE, GCS_BUCKET, BIG_QUERY_DATASET, BIG_QUERY_TABLE):
-    
-    dataframe_of_multi_sheet = pd.read_excel(
-        INPUT_DATA_PATH, 
-        sheet_name=None, 
-        header=None
-    )
-    sheets = dataframe_of_multi_sheet.keys()
-    storage_client = storage.Client.from_service_account_json(
-        GOOGLE_SERVICE_ACCOUNT_FILE
-    )
-    client = bigquery.Client.from_service_account_json(
-        GOOGLE_SERVICE_ACCOUNT_FILE
-    )
+def load_bigquery_to_excel_sheets(INPUT_DATA_PATH, OUTPUT_DATA_PATH, PROJECT_ID, GOOGLE_SERVICE_ACCOUNT_FILE, GCS_BUCKET, BIG_QUERY_DATASET, BIG_QUERY_TABLE, MULTI_SHEET=False):
+    if MULTI_SHEET == True:
+        dataframe_of_multi_sheet = pd.read_excel(
+            INPUT_DATA_PATH, 
+            sheet_name=None, 
+            header=None
+        )
+        sheets = dataframe_of_multi_sheet.keys()
+        storage_client = storage.Client.from_service_account_json(
+            GOOGLE_SERVICE_ACCOUNT_FILE
+        )
+        client = bigquery.Client.from_service_account_json(
+            GOOGLE_SERVICE_ACCOUNT_FILE
+        )
 
-    if not os.path.isdir(OUTPUT_DATA_PATH):                                                           
-        os.mkdir(OUTPUT_DATA_PATH)
+        if not os.path.isdir(OUTPUT_DATA_PATH):                                                           
+            os.mkdir(OUTPUT_DATA_PATH)
 
-    for i, sheet in enumerate(sheets):
-        target_dataframe = dataframe_of_multi_sheet.get(sheet)
+        for i, sheet in enumerate(sheets):
+            target_dataframe = dataframe_of_multi_sheet.get(sheet)
+            new_columns = ["col_{}".format(i) for i in target_dataframe.columns]
+            target_dataframe.columns = new_columns
+            target_dataframe = target_dataframe.astype(str)
+            server_output_path = "{}/{}_{}.csv".format(
+                OUTPUT_DATA_PATH, 
+                BIG_QUERY_TABLE,
+                i+1
+            )
+            target_dataframe.to_csv(
+                server_output_path,
+                index=False, 
+                encoding='utf-8-sig'
+            )
+            path_suffix = "pupledog/{}_{}.csv".format(
+                BIG_QUERY_TABLE,
+                i+1
+            )
+            upload_blob(
+                GCS_BUCKET, 
+                storage_client,
+                source_file_name=server_output_path, 
+                destination_blob_name=path_suffix
+            )
+            print("{} saving into GCS is done".format(
+                i+1
+            ))
+            load_gcs_to_bigquery(
+                client,
+                uri='gs://{}/{}'.format(
+                    GCS_BUCKET, 
+                    path_suffix
+                ), 
+                table_id = '{}.{}.{}_{}'.format(
+                    PROJECT_ID, 
+                    BIG_QUERY_DATASET,
+                    BIG_QUERY_TABLE,
+                    i+1
+                )
+            )
+            print("{} saving into BigQuery is done".format(i+1))
+
+    else:
+        dataframe_of_single_sheet = pd.read_excel(
+            INPUT_DATA_PATH, 
+            header=None
+        )
+        storage_client = storage.Client.from_service_account_json(
+            GOOGLE_SERVICE_ACCOUNT_FILE
+        )
+        client = bigquery.Client.from_service_account_json(
+            GOOGLE_SERVICE_ACCOUNT_FILE
+        )
+        if not os.path.isdir(OUTPUT_DATA_PATH):                                                           
+            os.mkdir(OUTPUT_DATA_PATH)
+
+        target_dataframe = dataframe_of_single_sheet
         new_columns = ["col_{}".format(i) for i in target_dataframe.columns]
         target_dataframe.columns = new_columns
         target_dataframe = target_dataframe.astype(str)
-        server_output_path = "{}/{}_{}.csv".format(
+        server_output_path = "{}/{}.csv".format(
             OUTPUT_DATA_PATH, 
-            BIG_QUERY_TABLE,
-            i+1
+            BIG_QUERY_TABLE
         )
         target_dataframe.to_csv(
             server_output_path,
             index=False, 
             encoding='utf-8-sig'
         )
-        path_suffix = "pupledog/{}_{}.csv".format(
-            BIG_QUERY_TABLE,
-            i+1
+        path_suffix = "pupledog/{}.csv".format(
+            BIG_QUERY_TABLE
         )
         upload_blob(
             GCS_BUCKET, 
@@ -72,37 +126,40 @@ def load_bigquery_to_excel_sheets(INPUT_DATA_PATH, OUTPUT_DATA_PATH, PROJECT_ID,
             source_file_name=server_output_path, 
             destination_blob_name=path_suffix
         )
-        print("{} saving into GCS is done".format(
-            i+1
-        ))
+        print("saving into GCS is done")
         load_gcs_to_bigquery(
             client,
             uri='gs://{}/{}'.format(
                 GCS_BUCKET, 
                 path_suffix
             ), 
-            table_id = '{}.{}.{}_{}'.format(
+            table_id = '{}.{}.{}'.format(
                 PROJECT_ID, 
                 BIG_QUERY_DATASET,
-                BIG_QUERY_TABLE,
-                i+1
+                BIG_QUERY_TABLE
             )
         )
-        print("{} saving into BigQuery is done".format(i+1))
+        print("saving into BigQuery is done")
+
+
+
+
+
 
 
 def main():
 
     # INPUT_DATA_PATH = "./data/wine_product.xlsx"  # Multi Sheet Excel File 
     INPUT_DATA_PATH = "./data/구독 배정 회원 리스트(21년)_20년 10월 이후.xlsx"
-    INPUT_DATA_PATH = "./data/구독 배정 회원 리스트(20년)_10월이전.xlsx"
+    INPUT_DATA_PATH = "./data/210810-구독 8월 정기2주차-발주요청서-0805.xlsx"
     
     OUTPUT_DATA_PATH = "./data_output"  # 아무거나 
     PROJECT_ID = "utopian-surface-268707"
     GOOGLE_SERVICE_ACCOUNT_FILE="./config/{}-service-account.json".format(PROJECT_ID) # Service Account File Name
     GCS_BUCKET = "automl_usc_inseon" # Bucket Name 
     BIG_QUERY_DATASET = "IS_USC" # BigQuery Dataset name
-    BIG_QUERY_TABLE = "ASSIGNMENT_BEFORE" # BigQuery Table name / Name 
+    BIG_QUERY_TABLE = "INVETORY" # BigQuery Table name / Name 
+    MULTI_SHEET = False
 
     load_bigquery_to_excel_sheets(
         INPUT_DATA_PATH, 
@@ -111,7 +168,8 @@ def main():
         GOOGLE_SERVICE_ACCOUNT_FILE, 
         GCS_BUCKET, 
         BIG_QUERY_DATASET,
-        BIG_QUERY_TABLE
+        BIG_QUERY_TABLE,
+        MULTI_SHEET
     )
 
 
